43542ec7e01a029ba7500708a350252ab860b4e5
FdelMazo <fdelmazo@fi.uba.ar>
2020-10-12 23:46:38 -0300

    Add safe copy of local commit hook

--- grammaticommit/grammaticommit.py
+++ grammaticommit/grammaticommit.py
@@ -1,11 +1,19 @@
 from shutil import copyfile
 import os, stat
 
+src = ["grammaticommit", "commit-msg"]
+local_dst = [".git", "hooks", "commit-msg"]
+
 def install(globally):
 	if globally:
 		print("I should install myself globally")
 
 	else:
-		copyfile("grammaticommit/commit-msg", ".git/hooks/commit-msg")
-		mode = os.stat('.git/hooks/commit-msg')
-		os.chmod('.git/hooks/commit-msg', mode.st_mode | stat.S_IEXEC)
+		if os.path.exists(os.path.join(*local_dst)):
+			print("You already have a commit-msg hook installed in this repository!")
+			print(f"It is placed on {os.path.join(*local_dst)}")
+			print("Delete it before installing grammaticommit!")
+			return
+		copyfile(os.path.join(*src), os.path.join(*local_dst))
+		mode = os.stat(os.path.join(*local_dst))
+		os.chmod(os.path.join(*local_dst), mode.st_mode | stat.S_IEXEC)
